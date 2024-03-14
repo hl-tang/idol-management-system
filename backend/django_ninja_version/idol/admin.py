55b5b51b9cfeb24bfcc419da5eb0ee47f637d55b
hl_tang <hltang@protonmail.com>
2024-03-14 22:59:41 +0900

    define database models

--- backend/django_ninja_version/idol/admin.py
+++ backend/django_ninja_version/idol/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Group, Idol, CD
+
+admin.site.register(Group)
+admin.site.register(Idol)
+admin.site.register(CD)
--- backend/django_ninja_version/idol/models.py
+++ backend/django_ninja_version/idol/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return f"{self.name}"
+
+class Idol(models.Model):
+    name = models.CharField(max_length=50)
+    birthday = models.DateField()
+    birthplace = models.CharField(max_length=10, db_comment="出身地")
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True)
+    profile_image = models.ImageField(null=True , blank=True)
+    def __str__(self):
+        return f"idol: {self.name}"
+
+class CD(models.Model):
+    title = models.CharField(max_length=50)
+    release_date = models.DateField()
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
